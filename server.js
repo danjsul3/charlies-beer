a86ca8dc4381a3b79074e790127b4f1670316963
Danny Sullivan <danny@sullivancosulting.org>
2021-08-09 07:44:05 -0400

    commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mustacheExpress = require ('mustache-express');
+const app = express();
+const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const dbUrl = 'mongodb://localhost:27017/data';
+
+const beerData = require('./public/beers.json')
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+    MongoClient.connect(dbUrl, async (err, db) => {
+        var userData = beerData
+        // var userData = await db.collection('beers').find({}).toArray();
+        response.render('index', {userData: userData})
+    });
+});
+
+app.get('/user/:id', (request, response) => {
+  MongoClient.connect(dbUrl, async (err, db) => {
+      var userData = await db.collection('beers').find({id: parseInt(request.params.id)}).toArray();
+      console.log(userData[0]);
+      response.render('user-page', userData[0]);
+  });
+});
+
+app.get('/unemployed', (request, response) => {
+    MongoClient.connect(dbUrl, async (err, db) => {
+        var userData = await db.collection('beers').find({job: null}).toArray();
+        response.render('employment', {userData:userData})
+    });
+});
+
+app.get('/employed', (request, response) => {
+    MongoClient.connect(dbUrl, async (err, db) => {
+        var userData = await db.collection('beers').find({job: { $ne: null}}).toArray();
+        response.render('employment', {userData: userData})
+    });
+});
+
+app.listen(3000);
